54e6c6d42d00ba5291cf3a9d054bba68a898e9ba
Andy Pratama Samudera <andypratamasamudera@gmail.com>
2026-05-19 16:17:16 +0700

    fix error build

--- src/app.ts
+++ src/app.ts
@@ -7,12 +7,16 @@ const port = Number(process.env.PORT) || 3000;
 
 app.set('view engine', 'ejs');
 
-app.set('views', path.join(__dirname, 'views'));
+const isDev = __dirname.endsWith('src') || __dirname.includes('/src');
+const viewsPath = isDev ? path.join(__dirname, '../views') : path.join(__dirname, 'views');
+const publicPath = isDev ? path.join(__dirname, '../public') : path.join(__dirname, 'public');
+
+app.set('views', viewsPath);
 
 app.use(express.json({ limit: '512kb' }));
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(publicPath));
 
 registerRoutes(app);
 
